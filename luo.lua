76c57b88bc655843807d213a14708a5efa557c0f
JairoAdelRio <59889219+JairoAdelRio@users.noreply.github.com>
2021-05-10 19:56:03 -0500

    Add files via upload

---
+++ luo.lua
@@ -0,0 +1,44 @@
+local math   = math
+local floor  = math.floor 
+local sqrt   = math.sqrt
+
+function primesieve(N)
+    local result = {2, 3}
+    if N < 2 then return {} end
+    if N < 3 then return {2} end
+    if N < 4 then return {2, 3} end
+    local c, k, t, q, M, S
+    c = 0
+    k = 1 
+    t = 2
+    q = floor(sqrt(N)/3)
+    M = N // 3
+    S = {}
+    local step = 2
+    local first = 5
+    while first <= N do
+        S[#S+1] = first 
+        first = first + step
+        step = 6 - step
+    end
+    for i = 1, q, 1 do
+        k = 3 - k 
+        c = c + 4*k*i
+        local j = c
+        local ij = 2*i*(3-k)+1
+        t = t + 4*k
+        if S[i] ~= 0 then
+            while j <= M do
+                S[j] = 0
+                j = j + ij
+                ij = t - ij
+            end
+        end
+    end
+    for i = 1, #S do
+        if S[i] > 0 then
+            result[#result + 1] = S[i] 
+        end
+    end
+    return result
+end
